2390b7f22970bd03223203e01b42fb9d5e845327
Sanket Vyawahare <srvyawahare18@gmail.com>
2025-10-15 14:55:55 +0530

    Added POC microservice for exprimentation

---
+++ poc/poc-microservice/poc-microservice/src/main/java/com/edenlens/poc/PocMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.edenlens.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMicroserviceApplication.class, args);
+	}
+
+}
